eec4a47f60e9405b0a1c26b9efe10b2217a634ee
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-09 23:42:56 +0400

    fix types

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import abort, jsonify
+from flask import Response, abort, jsonify
+from flask.typing import ResponseReturnValue
 
 from api.v1.views import app_views
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
-def status() -> str:
+def status() -> Response:
     """GET /api/v1/status
     Return:
       - the status of the API
@@ -16,7 +17,7 @@ def status() -> str:
 
 
 @app_views.route("/stats/", strict_slashes=False)
-def stats() -> str:
+def stats() -> Response:
     """GET /api/v1/stats
     Return:
       - the number of each objects
@@ -29,7 +30,7 @@ def stats() -> str:
 
 
 @app_views.route("/unauthorized", strict_slashes=False)
-def unauthorized() -> None:
+def unauthorized() -> ResponseReturnValue:
     """GET /api/v1/unauthorized
     Return:
         - Unaithorized Error.
