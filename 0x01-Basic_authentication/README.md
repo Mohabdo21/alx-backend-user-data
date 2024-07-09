d9db39a3a77d9367aa24348c6f3e3ace8e892df0
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-09 23:31:30 +0400

    1. Error handler: Unauthorized

---
+++ 0x01-Basic_authentication/README.md
@@ -0,0 +1,11 @@
+# 0x01. Basic Authentication
+
+This project is part of the Back-end Authentication curriculum. The main objective is to understand and implement advanced concepts of basic authentication.
+
+## Learning Objectives
+
+- Understand what authentication means and its importance in web applications.
+- Learn about Base64 encoding and how it is used in the context of web development.
+- Understand how to encode a string in Base64 using Python.
+- Learn what Basic Authentication means and how it is implemented in web applications.
+- Understand how to send the Authorization header in HTTP requests for Basic Authentication.
---
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+import os
+from os import getenv
+from typing import Any, Literal
+
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> tuple[Any, Literal[401]]:
+    """Unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """Not found handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
