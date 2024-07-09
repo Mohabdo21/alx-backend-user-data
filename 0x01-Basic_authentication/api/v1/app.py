1b65c828db343a2e9f7aa829c2cf6690d2cc4973
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-09 23:51:03 +0400

    Original state

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,30 +2,22 @@
 """
 Route module for the API
 """
-import os
 from os import getenv
-from typing import Any, Literal
-
-from flask import Flask, abort, jsonify, request
-from flask_cors import CORS, cross_origin
-
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler"""
-    return jsonify({"error": "Unauthorized"}), 401
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
