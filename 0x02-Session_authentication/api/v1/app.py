37d5b59e10f0d85d2583dbedbe00150a5abf0381
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-11 22:13:01 +0400

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,11 +35,12 @@ def before_request_fn():
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
             if auth_header is None:
                 abort(401)
+            user = auth.current_user(request)
             if user is None:
                 abort(403)
+            setattr(request, "current_user", user)
 
 
 @app.errorhandler(404)
