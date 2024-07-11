a70641dda720e2929f1c18d8fdd686b8429e9cc9
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-12 03:56:51 +0400

    Debug

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -15,20 +15,14 @@ class SessionDBAuth(SessionExpAuth):
         """Creates a new session for a user and saves it in the database."""
         session_id = super().create_session(user_id)
         if session_id:
-            created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-            UserSession(user_id=user_id, session_id=session_id,
-                        created_at=created_at).save()
+            UserSession(user_id=user_id, session_id=session_id).save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """Retrieves the user ID associated with a session ID."""
         if session_id:
-            users_session = UserSession.search({"session_id": session_id})
-            if users_session:
-                user_session = users_session[0]
-                session_age = datetime.now() - user_session.created_at
-                if session_age <= timedelta(seconds=self.session_duration):
-                    return user_session.user_id
+            user_id = super().user_id_for_session_id(session_id)
+            return user_id
         return None
 
     def destroy_session(self, request=None):
@@ -36,7 +30,8 @@ class SessionDBAuth(SessionExpAuth):
         if request:
             session_id = self.session_cookie(request)
             user_session = UserSession.search({"session_id": session_id})
-            if user_session:
+            if user_session and user_session is not []:
                 user_session[0].remove()
+                super().destroy_session(request)
                 return True
         return False
