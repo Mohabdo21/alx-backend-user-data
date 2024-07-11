b62fb5144fbc7b4af388d260b39710a560e5b28f
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-11 22:24:33 +0400

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module to apply session authentication logic.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session Authentication logic"""
+
+    pass
