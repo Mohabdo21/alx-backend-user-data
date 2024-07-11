f60652aebb55207579d43a233ae8c0e3bb92b25c
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-11 22:57:35 +0400

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,10 +35,11 @@ def before_request_fn():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            if auth_header is None:
+            if auth_header is None and auth.session_cookie(request) is None:
                 abort(401)
             user = auth.current_user(request)
             if user is None:
