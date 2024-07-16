1cb1e22ba6c990a3a0d82c1ed012e15f9bebf455
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-16 17:36:30 +0400

    fix the raising

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -74,7 +74,7 @@ class DB:
         try:
             self._validate_attributes(kwargs)
         except ValueError:
-            raise IndentationError
+            raise InvalidRequestError
 
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
