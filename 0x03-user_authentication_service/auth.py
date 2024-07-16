ce729ecfdf567221862620f4c59c54bf3373853f
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-07-16 18:28:34 +0400

    not validate update password args

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -130,8 +130,8 @@ class Auth:
         If the reset token is not valid or the password is not provided,
         a ValueError is raised.
         """
-        if not reset_token or not password:
-            return None
+        # if not reset_token or not password:
+        #     return None
 
         try:
             user = self._db.find_user_by(reset_token=reset_token)
